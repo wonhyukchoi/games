1e0e57a13d00e241d13da606d80efbea667eea0d
Wonhyuk Choi <wonhyuk.choi@columbia.edu>
2020-07-13 22:27:36 +0900

    finished 2person tic tac toe

--- tic-tac-toe/C/main.c
+++ tic-tac-toe/C/main.c
@@ -31,14 +31,44 @@ bool board_not_full(char array[SIZE][SIZE]){
     return false;
 }
 
-bool has_won(char array[SIZE][SIZE]){
+bool has_won(char array[SIZE][SIZE], char *winner){
+
+    bool won;
+
+    // Horizontal
+    for(int i=0; i<SIZE; i++){
+        *winner = array[i][0];
+        won = true;
+        for(int j=1; j<SIZE; j++){
+            char elem = array[i][j];
+            if(elem != *winner || elem == EMPTY) {
+                won=false; break;
+            }
+        }
+        if(won) return true;
+    }
+
+    // Vertical
+    for(int j=0; j<SIZE; j++){
+        *winner = array[0][j];
+        won = true;
+        for(int i=1; i<SIZE; i++){
+            char elem = array[i][j];
+            if(elem != *winner || elem == EMPTY) {
+                won=false; break;
+            }
+        }
+        if(won) return true;
+    }
+
     return false;
+
 }
 
 /*
  * Gets long from stdin.
- * Prompts for retry on incorrect input.
- * Updates
+ * Reprompts on wrong input.
+ * Uses recursion b/c I am lazy.
  */
 long get_int(){
     int max_len = 80;
@@ -59,12 +89,26 @@ long get_int(){
 
 /*
  * Gets row and column from stdin.
+ * Re-prompts input if the block is already occupied.
+ * (using recursion because I am lazy).
+ * Inputs are assumed to be one-indexed.
  */
-void get_coords(long* row, long* column){
+void get_coords(long* row, long* column
+               ,char array[SIZE][SIZE]){
+
     printf("Enter your desired row:");
     *row = get_int();
     printf("Enter your desired column:");
     *column = get_int();
+
+    // Checks if 2d array is already filled.
+    // Converts one-indexing to zero-indexing.
+    if(array[*row-1][*column-1] != EMPTY){
+        printf("\nThat place is already filled."
+               "Please try again.\n");
+        get_coords(row, column, array);
+    }
+
 }
 
 void play_game(){
@@ -72,18 +116,28 @@ void play_game(){
     char field[SIZE][SIZE] = {EMPTY,EMPTY,EMPTY
                              ,EMPTY,EMPTY,EMPTY
                              ,EMPTY,EMPTY,EMPTY};
-    bool playerX = false;
+    bool player1 = true;
+    char winner_symbol;
+    char* winner;
 
     while(board_not_full(field)){
-        show_field(field);
 
         long row, column;
-        get_coords(&row, &column);
+        get_coords(&row, &column, field);
+
         // Don't use zero indexing for normal people.
-        if(playerX) field[row-1][column-1] = 'X';
+        // If it's player 1, update it with 'X', otherwise 'O'.
+        if(player1) field[row - 1][column - 1] = 'X';
         else field[row-1][column-1] = 'O';
 
-        playerX = !playerX;
+        // Break loop & exit if game has been won.
+        if(has_won(field, &winner_symbol)){
+            winner = (winner_symbol=='X') ? "Player 1" : "Player 2";
+            printf("%s has won!", winner);
+            break;
+        }
+        player1 = !player1;
+        show_field(field);
     }
 
     printf("\n *** Thanks for playing! ***\n");
